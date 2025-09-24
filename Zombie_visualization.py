e5f1e10d096a8ce12432f6d31db30e06e93a9e80
acenyioko <aenyioko090@gmail.com>
2025-09-24 15:04:47 -0500

    Added a visualization for bite size

--- Zombie_visualization.py
+++ Zombie_visualization.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # imoprt zombie training dataset
 zombie_df = pd.read_csv('training.csv')
 
-X = zombie_df.drop(columns=['bite_size','green_blood_count','infected_family','location'])
+x = zombie_df.drop(columns=['bite_size','green_blood_count','infected_family','location'])
 y = zombie_df['is_zombie']
 
-print(X)
-
-
+sns.histplot(zombie_df, x='bite_size', hue='is_zombie', kde=True)
+plt.title('Distribution of Bite Size by Zombie Status')
+plt.show()
